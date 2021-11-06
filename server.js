a32df3d0866ab01c4bbdf437667aaf6275f64a5d
Olsen <chrisolsen1993@gmail.com>
2021-11-06 07:17:16 -0400

    writting to file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require ("path")
 const fs = require("fs");
 const uuid = require("./helpers/uuid")
+const notes =require('./db/notes');
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -18,8 +19,44 @@ app.get ("/notes",(req, res) =>{
 })
 
 app.get("/api/notes", (req, res) => {
-    return JSON.parse(fs.readFileSync("./db/notes.json", "utf-8"));
+//     function readNote(){
+//         return JSON.parse(fs.readFileSync("./db/notes.json", "utf-8"));
+//     }
+//    const notes= readNote()
+    res.status(200).json(notes);
   });
+  
+app.post("/api/notes", (req, res) => {
+const {noteTitle, noteText}=req.body
+if (noteTitle && noteText){
+const newNote = {
+    noteTitle,
+    noteText,
+    id: uuid(),
+}
+const parsedNotes = JSON.parse(notes);
+parsedNotes.push(newNote)
 
+// Write updated note back to the file
+fs.writeFile(
+    './db/notes.json',
+    JSON.stringify(parsedNotes, null, 4),
+    (writeErr) =>
+      writeErr
+        ? console.error(writeErr)
+        : console.info('Successfully updated notes!')
+  );
+
+const response = {
+    status: "success",
+    body: newNote,
+}
+console.log(response)
+res.status(201).json(response);
+
+} else {
+    res.status(500).json('Error in posting review');
+    }
+  })
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`))
