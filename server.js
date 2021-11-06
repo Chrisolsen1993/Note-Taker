49c889e7814f314e568d8f56c874952a1d0688e2
Olsen <chrisolsen1993@gmail.com>
2021-11-06 16:47:16 -0400

    got everything done

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const path = require ("path")
+const path = require("path")
 const fs = require("fs");
 const uuid = require("./helpers/uuid")
-const notes =require('./db/notes');
+// const notes = require('./db/notes');
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -10,53 +10,58 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// Create routes
-app.get("/", (req, res) =>{
+// Create routes html 
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
-app.get ("/notes",(req, res) =>{
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
-
+// api routes
 app.get("/api/notes", (req, res) => {
-//     function readNote(){
-//         return JSON.parse(fs.readFileSync("./db/notes.json", "utf-8"));
-//     }
-//    const notes= readNote()
+   const notes =readNote()
     res.status(200).json(notes);
-  });
-  
+});
+
 app.post("/api/notes", (req, res) => {
-const {noteTitle, noteText}=req.body
-if (noteTitle && noteText){
-const newNote = {
-    noteTitle,
-    noteText,
-    id: uuid(),
-}
-const parsedNotes = JSON.parse(notes);
-parsedNotes.push(newNote)
+    // console.log(req.body)
+    const { title, text } = req.body
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        }
+        const notes =readNote()
+        console.log(notes)
 
-// Write updated note back to the file
-fs.writeFile(
-    './db/notes.json',
-    JSON.stringify(parsedNotes, null, 4),
-    (writeErr) =>
-      writeErr
-        ? console.error(writeErr)
-        : console.info('Successfully updated notes!')
-  );
 
-const response = {
-    status: "success",
-    body: newNote,
-}
-console.log(response)
-res.status(201).json(response);
+        notes.push(newNote)
 
-} else {
-    res.status(500).json('Error in posting review');
+        // Write updated note back to the file
+        fs.writeFile(
+            './db/notes.json',
+            JSON.stringify(notes, null, 4),
+            (writeErr) =>
+                writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+        );
+
+        const response = {
+            status: "success",
+            body: newNote,
+        }
+        console.log(response)
+        res.status(201).json(response);
+
+    } else {
+        res.status(500).json('Error in posting review');
     }
-  })
+})
+
+function readNote(){
+    return JSON.parse(fs.readFileSync("./db/notes.json", "utf-8"));
+}
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`))
